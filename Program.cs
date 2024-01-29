becaf3949d9bbbfb1525354d79eb11d6065951b9
renson.gitonga <renson.gitonga@RENSON>
2024-01-29 08:23:00 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrderIdsAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fraudulentOrdersIds = new string[3];
+        }
+    }
+}
