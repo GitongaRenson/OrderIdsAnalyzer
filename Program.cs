ec28dd7168e2f26a195ccd194ca53497066992e4
renson.gitonga <rensongitonga34@gmail.com>
2024-01-29 09:32:04 +0300

    This is the new change

--- Program.cs
+++ Program.cs
@@ -6,7 +6,34 @@ namespace OrderIdsAnalyzer
     {
         static void Main(string[] args)
         {
+            /*
             string[] fraudulentOrdersIds = new string[3];
+            fraudulentOrdersIds[0] = "A123";
+            fraudulentOrdersIds[1] = "B456";
+            fraudulentOrdersIds[2] = "C789";
+            //fraudulentOrdersIds[3] = "D101";
+            */
+
+            string[] fraudulentOrdersIds = { "A123", "B456", "C789" };
+            //Accessing the values in the array.
+
+            Console.WriteLine($"First: {fraudulentOrdersIds[0]}");
+            Console.WriteLine($"Second: {fraudulentOrdersIds[1]}");
+            Console.WriteLine($"Third: {fraudulentOrdersIds[2]}");
+
+            //Reassigning a value to an index of the array
+            fraudulentOrdersIds[0] = "F000";
+
+            Console.WriteLine($"There are {fraudulentOrdersIds.Length} fraudulent orders to be processes");
+            Console.WriteLine($"Reassign First: {fraudulentOrdersIds[0]}");
+
+
+            string[] names = { "Rowena", "Robin", "Bao" };
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
