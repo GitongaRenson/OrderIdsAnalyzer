d3d9afa7e20f60afbed891fa347430eacc2a41b8
renson.gitonga <rensongitonga34@gmail.com>
2024-02-01 17:31:49 +0300

    Practiced on switch and foreach loop.

--- Program.cs
+++ Program.cs
@@ -2,12 +2,12 @@
 
 namespace OrderIdsAnalyzer
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            
-             //The following code creates five random OrderIDs to test the fraud detection process.
+
+            /* //The following code creates five random OrderIDs to test the fraud detection process.
             //OrderIds consist of a letter from A to E and a three digit number, Ex A123.
            
             Random random = new Random();
@@ -71,9 +71,111 @@ namespace OrderIdsAnalyzer
                     letterCount++;
                 }
             }
+            int firstValue = 600;
+            int secondValue = 450;
+            int largerValue;
+
+           largerValue =Math.Max(firstValue,secondValue);
+
             //string newMessage = new string(message);
+            Console.WriteLine(largerValue);
             Console.WriteLine(message);
-            Console.WriteLine($"'o' appears {letterCount} times.");
+            Console.WriteLine($"'o' appears {letterCount} times."); 
+            
+            int[] numbers = { 4, 8, 15, 16, 23, 42 };
+            int total =0;
+            bool found;
+            foreach (int number in numbers)
+            {
+                
+
+                total += number;
+              
+                if (number == 42)
+                {
+                   found = true;
+                    Console.WriteLine("Set contains 42");
+                }
+                
+            }
+         
+            Console.WriteLine($"Total: {total}");
+           
+            var employeeLevel1 = Console.ReadLine();
+            int employeeLevel = Convert.ToInt32(employeeLevel1);
+            string employeeName = "John Smith";
+
+            string title = "";
+            switch (employeeLevel)
+            {
+                case 100:
+                    title = "Junior Associate";
+                    break;
+
+                case 200:
+                    title = "Senior Associate";
+                    break;
+                case 300:
+                    title = "Manager";
+                    break;
+                default:
+                    title = " Associate";
+                    break;
+            }
+            Console.WriteLine($"{employeeName}, {title}");
+            */
+            // SKU = Stock Keeping Unit.
+            // SKU value format: <product #>-<2-letter color code>-<size code>
+            string sku = "01-MN-L";
+            string[]product = sku.Split('-');
+
+            string type = "";
+            string color = "";
+            string size = "";
+            switch (product[0])
+            {
+                case "01":
+                    type = "Sweat shirt";
+                    break;
+                case "02":
+                    type = "T-shirt";
+                    break;
+                case "03":
+                    type = "Sweat pants";
+                    break;
+                default:
+                    type = "Other";
+                    break;
+            }
+            switch (product[1])
+            {
+                case "BL":
+                    color = "Black";
+                    break;
+                case "MN":
+                    color = "Maroon";
+                    break;
+                default:
+                    color = "White";
+                    break;
+            }
+            switch(product[2])
+            {
+                case "L":
+                    size = "Large";
+                    break;
+                case "S":
+                    size = "Small";
+                    break;
+                case "M":
+                    size = "Medium";
+                    break;
+                default:
+                    size = "One Size Fits All";
+                    break;
+            }
+            Console.WriteLine($"Product: {size} {color} {type}");
+
         }
     }
 }
---
+++ RandomNumbers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OrderIdsAnalyzer
+{
+    internal class RandomNumbers
+    {
+        int firstValue = 600;
+        int secondValue = 450;
+        int largerValue;
+
+
+    }
+}
